28427497ecb3c322654791dfb5eb2d6ca22d2978
Joe Gannon <joegannon90@yahoo.com>
2017-07-17 02:10:03 -0400

    added functionality to construct properties from a supplied function

---
+++ Construktion.Tests/Simple/CustomPropertyValueBlueprintTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Construktion.Tests.Simple
+{
+    using System;
+    using System.Reflection;
+    using Blueprints.Simple;
+    using Shouldly;
+    using Xunit;
+
+    public class CustomPropertyValueBlueprintTests
+    {
+        [Fact]
+        public void should_match_defined_convention()
+        { 
+            var blueprint = new CustomPropertyValueBlueprint(x => x.Name.Equals("HireDate"), () => DateTime.MaxValue);
+
+            var matches = blueprint.Matches(new ConstruktionContext(typeof(Foo).GetProperty(nameof(Foo.HireDate))));
+
+            matches.ShouldBe(true);
+        }
+
+        [Fact]
+        public void should_construct_from_value()
+        {
+            var blueprint = new CustomPropertyValueBlueprint(x => x.Name.Equals("HireDate"), () => DateTime.MaxValue);
+
+            var result = blueprint.Construct(new ConstruktionContext(typeof(Foo).GetProperty(nameof(Foo.HireDate))),
+                Default.Pipeline);
+
+            result.ShouldBeOfType<DateTime>().ShouldBe(DateTime.MaxValue);
+        }
+
+        public class Foo
+        {
+            public DateTime HireDate { get; set; }
+        }
+    }
+}
---
+++ Construktion/Blueprints/Simple/CustomPropertyValueBlueprint.cs
@@ -0,0 +1,27 @@
+﻿namespace Construktion.Blueprints.Simple
+{
+    using System;
+    using System.Reflection;
+
+    public class CustomPropertyValueBlueprint : Blueprint
+    {
+        private readonly Func<PropertyInfo, bool> _convention;
+        private readonly Func<object> _value;
+
+        public CustomPropertyValueBlueprint(Func<PropertyInfo, bool> convention, Func<object> value)
+        {
+            _convention = convention;
+            _value = value;
+        }
+
+        public bool Matches(ConstruktionContext context)
+        {
+            return context.PropertyInfo != null && _convention(context.PropertyInfo);
+        }
+
+        public object Construct(ConstruktionContext context, ConstruktionPipeline pipeline)
+        {
+            return _value();
+        }
+    }
+}
