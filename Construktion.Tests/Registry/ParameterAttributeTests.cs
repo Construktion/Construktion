d65820707637208aa57439456da9d60b0c0d809f
Joe Gannon <joegannon90@yahoo.com>
2017-11-09 22:02:28 -0500

    split attribtue tests into new files

---
+++ Construktion.Tests/Registry/ParameterAttributeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Shouldly;
+using Xunit;
+
+namespace Construktion.Tests.Registry
+{
+    public class ParameterAttributeTests
+    {
+        [Fact]
+        public void should_register_parameter_attribute_blueprint()
+        {
+            var registry = new ConstruktionRegistry().AddParameterAttribute<Set>(x => x.Value);
+
+            var parameterInfo =
+                typeof(ParameterAttributeTests).GetMethod(nameof(ParameterTest))
+                                                   .GetParameters()
+                                                   .Single();
+
+            var parameter = (string)new Construktion().With(registry).Construct(parameterInfo);
+
+            parameter.ShouldBe("Set");
+        }
+
+        public void ParameterTest([Set("Set")] string parameter) { }
+
+        public class Set : Attribute
+        {
+            public string Value { get; }
+
+            public Set(string value)
+            {
+                Value = value;
+            }
+        }
+    }
+}
---
+++ Construktion.Tests/Registry/PropertyAttributeUsageTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace Construktion.Tests.Registry
+{
+    public class PropertyAttributeUsageTests
+    {
+        [Fact]
+        public void should_register_property_attribute_blueprint()
+        {
+            var registry = new ConstruktionRegistry().AddPropertyAttribute<Set>(x => x.Value);
+
+            var foo = new Construktion().With(registry).Construct<Foo>();
+
+            foo.WithAttribute.ShouldBe("Set");
+        }
+
+        public class Foo
+        {
+            [Set("Set")]
+            public string WithAttribute { get; set; }
+        }
+
+        public class Set : Attribute
+        {
+            public string Value { get; }
+
+            public Set(string value)
+            {
+                Value = value;
+            }
+        }
+    }
+}
