21ef59b23ab9eb457e24133b234e21f72893cf3e
Joe Gannon <joegannon90@yahoo.com>
2017-11-09 21:13:37 -0500

    split the pipeline and default pipeline into separate files

--- Construktion/ConstruktionPipeline.cs
+++ Construktion/ConstruktionPipeline.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Construktion.Blueprints;
 
 namespace Construktion
 {
@@ -25,60 +22,5 @@ namespace Construktion
         /// <param name="requestType"></param>
         /// <param name="value"></param>
         void Inject(Type requestType, object value);
-    }
-
-    internal class DefaultConstruktionPipeline : ConstruktionPipeline
-    {
-        private readonly Construktion _construktion;
-        public ConstruktionSettings Settings { get; }
-
-        public DefaultConstruktionPipeline() : this(new Construktion()) { }
-
-        public DefaultConstruktionPipeline(Construktion construktion)
-        {
-            _construktion = construktion;
-            Settings = construktion.Registry.ToSettings();
-        }
-
-        public object Send(ConstruktionContext context)
-        {
-            var blueprint = Settings.Blueprints.First(x => x.Matches(context));
-
-            var result = Construct(context, blueprint);
-
-            return result;
-        }
-
-        public void Inject(Type type, object value)
-        {
-            _construktion.Inject(type, value);
-        }
-
-        private readonly List<Type> _underConstruction = new List<Type>();
-
-        private object Construct(ConstruktionContext context, Blueprint blueprint)
-        {
-            if (RecurssionDetected(context))
-            {
-                return Settings.ThrowOnRecurrsion
-                    ? throw new Exception($"Recursion Detected: {context.RequestType.FullName}")
-                    : default(object);
-            }
-
-            _underConstruction.Add(context.RequestType);
-
-            var result = blueprint.Construct(context, this);
-
-            _underConstruction.Remove(context.RequestType);
-
-            return result;
-        }
-
-        private bool RecurssionDetected(ConstruktionContext context)
-        {
-            var depth = _underConstruction.Count(x => context.RequestType == x);
-
-            return depth > Settings.RecurssionDepth || (depth > 0 && Settings.ThrowOnRecurrsion);
-        }
     }
 }
---
+++ Construktion/DefaultConstruktionPipeline.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Construktion.Blueprints;
+
+namespace Construktion
+{
+    internal class DefaultConstruktionPipeline : ConstruktionPipeline
+    {
+        //todo remove circular reference #
+        private readonly Construktion _construktion;
+        public ConstruktionSettings Settings { get; }
+
+        public DefaultConstruktionPipeline() : this(new Construktion()) { }
+
+        public DefaultConstruktionPipeline(Construktion construktion)
+        {
+            _construktion = construktion;
+            Settings = construktion.Registry.ToSettings();
+        }
+
+        public object Send(ConstruktionContext context)
+        {
+            var blueprint = Settings.Blueprints.First(x => x.Matches(context));
+
+            var result = Construct(context, blueprint);
+
+            return result;
+        }
+
+        public void Inject(Type type, object value)
+        {
+            _construktion.Inject(type, value);
+        }
+
+        private readonly List<Type> _underConstruction = new List<Type>();
+
+        private object Construct(ConstruktionContext context, Blueprint blueprint)
+        {
+            if (RecurssionDetected(context))
+            {
+                return Settings.ThrowOnRecurrsion
+                    ? throw new Exception($"Recursion Detected: {context.RequestType.FullName}")
+                    : default(object);
+            }
+
+            _underConstruction.Add(context.RequestType);
+
+            var result = blueprint.Construct(context, this);
+
+            _underConstruction.Remove(context.RequestType);
+
+            return result;
+        }
+
+        private bool RecurssionDetected(ConstruktionContext context)
+        {
+            var depth = _underConstruction.Count(x => context.RequestType == x);
+
+            return depth > Settings.RecurssionDepth || (depth > 0 && Settings.ThrowOnRecurrsion);
+        }
+    }
+}
